bda53f61e8b2cc01a39644331eddf6fb8f2dc825
vldmrklnk <uladzimerkalenik@gmail.com>
2019-08-23 13:47:08 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunWithStringBuilder
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			StringBuilder sb = new StringBuilder("****Fantastic Games****");
+			sb.Append("\n");
+			sb.AppendLine("Half-Life");
+			sb.AppendLine("Morrowind");
+			sb.AppendLine("Deus Ex" + "2");
+			sb.AppendLine("System Shock");
+			Console.WriteLine((sb.ToString()));
+			sb.Replace("2", "Invisible War");
+			Console.WriteLine(sb.ToString());
+			Console.WriteLine("sb has {0} chars.", sb.Length); ;
+			Console.WriteLine();
+		}
+	}
+}
